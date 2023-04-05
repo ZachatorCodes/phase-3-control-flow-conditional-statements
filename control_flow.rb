44ad829697c0b021e92fee88013a696bb34bb825
Zach Schwartz <zachatorcodes@gmail.com>
2023-04-05 12:54:40 -0600

    finished lab

--- control_flow.rb
+++ control_flow.rb
@@ -7,8 +7,7 @@ def admin_login(username, password)
 end
 
 def hows_the_weather(temperature)
-  response =
-    if temperature < 40
+  response = if temperature < 40
       "It's brisk out there!"
     elsif temperature >= 40 && temperature <= 65
       "It's a little chilly out there!"
@@ -20,9 +19,28 @@ def hows_the_weather(temperature)
 end
 
 def fizzbuzz(num)
-  # your code here
+  response = if num % 3 == 0 && num % 5 == 0
+      "FizzBuzz"
+    elsif num % 3 == 0
+      "Fizz"
+    elsif num % 5 == 0
+      "Buzz"
+    else
+      num
+    end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "/"
+    num1 / num2
+  when "*"
+    num1 * num2
+  else
+    puts "Invalid operation!"
+  end
 end
